312e515180c2cdfcbe39999b009f7bf9879c333a
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-14 17:46:49 -0500

    added app.js with header in react

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="header-logo">
+                <img src="https://via.placeholder.com/40" alt="" />
+            </div>
+            <div className="header-title">Today</div>
+            <div className="header-buttons">
+                <button id="login">Login</button>
+                <button id="login">Sign Up</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
