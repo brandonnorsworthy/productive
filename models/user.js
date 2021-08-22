5c9d57fb3de59f8650ed2c65bcdd1061e4b734d6
Jacob Guiro <jguiro09@gmail.com>
2021-08-22 14:31:35 -0500

    User table created

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        trim: true,
+        match: [/.+@.+\..+/, "Make sure what you have typed is an email!"],
+        required: "Please enter an email"
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: "Please enter a password"
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
