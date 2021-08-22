84261faf518c3058652497c0a8195ff610e3c612
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-08-22 15:29:24 -0500

    props with obj

--- src/components/content/Habit.js
+++ src/components/content/Habit.js
@@ -6,24 +6,24 @@ function Habit(props) {
             <div className="card-body">
                 <div className="habit-col-a">
                     <span className="material-icons" unselectable="on" onselectstart="return false;"
-                        onmousedown="return false;">water_drop</span>
+                        onmousedown="return false;">{props.habit.icon}</span>
                 </div>
                 <div className="habit-col-b">
                     <div className="row">
                         <div>
-                            <h2>Lorem, ipsum dolor sit amet consectetur adipisicing</h2>
+                            <h2>{props.habit.habitName}</h2>
                         </div>
                         <div>
-                            <h2><b>0/1</b></h2>
+                            <h2><b>{props.habit.quantity.count}
+                            {props.habit.quantity.total}</b></h2>
                         </div>
                     </div>
                     <div className="row">
                         <div>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi placeat asperiores
-                                facere consectetur, molestias blanditiis.</p>
+                            <p>{props.habit.description}</p>
                         </div>
                         <div>
-                            <p>Glasses</p>
+                            <p>{props.habit.subject}</p>
                         </div>
                     </div>
                 </div>
