030586ce6ad8040c53f6298563668ea4c722ccab
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-23 19:21:06 -0500

    header title now changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,28 @@
 import React from 'react'
 
 function Header({ currentContent, handlePageChange }) {
-    function generateHeaderTitle() {
+    function generateHeaderTitle(text) {
         let str = 'error'
-        // switch (currentContent) {
-        //     case value:
-        //         break;
-        // }
+        switch (currentContent) {
+            case 'Create':
+                str = 'Create a Habit'
+                break;
+            case 'Stats':
+                str = 'Previous Stats'
+                break;
+            case 'Today':
+                str = 'Today'
+                break;
+            case 'Habits':
+                str = 'All Habits'
+                break;
+            case 'Settings':
+                str = 'Settings'
+                break;
+            default:
+                break;
+        }
 
-        console.log("cc",currentContent)
         return str;
     }
 
@@ -17,7 +31,7 @@ function Header({ currentContent, handlePageChange }) {
             <div className="header-logo">
                 <img src="https://via.placeholder.com/40" alt="" />
             </div>
-            <div className="header-title">{ generateHeaderTitle }</div>
+            <div className="header-title">{generateHeaderTitle(currentContent)}</div>
             <div className="header-buttons">
                 <button onClick={() => handlePageChange('login')} id="login">Login</button>
                 <button onClick={() => handlePageChange('signup')} id="signup">Sign&nbsp;Up</button>
