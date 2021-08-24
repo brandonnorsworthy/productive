103e2c35cc8b331d7252e18d7ee4c979d09abb9f
Brandon Norsworthy <brandonknorsworthy@gmail.com>
2021-08-23 19:17:58 -0500

    cleanup app.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-function Header({ handlePageChange }) {
+function Header({ currentContent, handlePageChange }) {
+    function generateHeaderTitle() {
+        let str = 'error'
+        // switch (currentContent) {
+        //     case value:
+        //         break;
+        // }
+
+        console.log("cc",currentContent)
+        return str;
+    }
+
     return (
         <header>
             <div className="header-logo">
                 <img src="https://via.placeholder.com/40" alt="" />
             </div>
-            <div className="header-title">Today</div>
+            <div className="header-title">{ generateHeaderTitle }</div>
             <div className="header-buttons">
                 <button onClick={() => handlePageChange('login')} id="login">Login</button>
                 <button onClick={() => handlePageChange('signup')} id="signup">Sign&nbsp;Up</button>
